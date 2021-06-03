c8a73af7e6d4b0ae755c456ebd3a76a86d0648fb
Thomas Philip <thomasphilipchry@gmail.com>
2021-06-03 18:14:17 +0530

    added mongodb connection

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,7 @@
 var express = require('express');
+const productHelpers = require('../helpers/product-helpers');
 var router = express.Router();
+var productHelper = require('../helpers/product-helpers')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -38,8 +40,18 @@ router.get('/add-product',function(req, res) {
 
 })
 router.post('/add-product',(req, res) => {
-  console.log(req.body);
-  console.log(req.files.Image);
+  productHelpers.addProduct(req.body,(id)=> {
+    let image = req.files.Image
+    console.log(id);
+    image.mv('./public/product-images/'+id+'.jpg',(err,done)=>{
+      if(!err){
+        res.render("admin/add-product")
+      } else {
+        console.log(err);
+      }
+    })
+    
+  })
 })
 
 module.exports = router;
