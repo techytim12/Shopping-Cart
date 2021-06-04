9646d09d69f12860c2ceaf6f652bd8bad4948989
Thomas Philip <thomasphilipchry@gmail.com>
2021-06-04 15:57:08 +0530

    user auth

--- routes/user.js
+++ routes/user.js
@@ -5,10 +5,10 @@ const userHelpers = require('../helpers/user-helpers')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
+  let user = req.session.user
+  console.log(user);
   productHelpers.getAllProducts().then((products)=> {
-    console.log(products);
-    res.render('user/view-products',{admin:false, products})
+    res.render('user/view-products',{products,user})
   })
 });
 
@@ -29,11 +29,18 @@ router.post('/signup', (req,res) => {
 router.post('/login', (req,res) => {
   userHelpers.doLogin(req.body).then((response) => {
     if(response.status){
+      req.session.loggedIn = true
+      req.session.user = response.user
       res.redirect('/')
     } else {
       res.redirect('/login')
     }
   })
+})
+
+router.get('/logout',(req,res)=> {
+  req.session.destroy()
+  res.redirect('/')
 })
 
 
